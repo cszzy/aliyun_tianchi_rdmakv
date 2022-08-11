2d88bbf8b04c7f5acbfbbec0989ba10078649f2c
zzy <1422872485@qq.com>
2022-08-11 09:38:08 +0000

    1

--- include/rdma_conn.h
+++ include/rdma_conn.h
@@ -17,8 +17,8 @@ namespace kv {
 
 #define RESOLVE_TIMEOUT_MS 5000
 
-#define CACHE_ENTRY_SIZE (1024)
-#define CACHE_ENTRY_MEM_SIZE (128 * 128)
+#define CACHE_ENTRY_SIZE (512)
+#define CACHE_ENTRY_MEM_SIZE (128 * 256)
 
 /* RDMA connection */
 class RDMAConnection {
