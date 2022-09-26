edcf3562a02f3e90413dae07de46e91fb976f0a2
zzy <142872485@qq.com>
2022-09-26 09:24:18 +0000

    1

--- include/kv_engine.h
+++ include/kv_engine.h
@@ -262,8 +262,8 @@ class LocalEngine : public Engine {
   // std::atomic<int> m_slot_cnt_{0}; /* Used to fetch the slot from hash_slot_array. */
   hash_map_t m_hash_map_[SHARDING_NUM];         /* Hash Map with sharding. */
   RDMAMemPool *m_mem_pool_[SHARDING_NUM];
-  // LRUCache *m_cache_[SHARDING_NUM];
-  ClockCache *m_cache_[SHARDING_NUM];
+  LRUCache *m_cache_[SHARDING_NUM];
+  // ClockCache *m_cache_[SHARDING_NUM];
 #ifdef USE_AES
   crypto_message_t m_aes_;
 #endif
